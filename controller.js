7656ed24c3f6f0a73a312604db4975871e9bb10f
Odomero <oshieodomero@gmail.com>
2022-03-04 07:15:17 +0100

    Updated Books API with POST request

--- controller.js
+++ controller.js
@@ -2,20 +2,76 @@ const pool = require('./db')
 const query = require('./queries')
 
 const getbookcat = (req, resp) => {
-    pool.query(query.getbookcat, (error,output) => {
+    pool.query(query.getBookCat, (error,output) => {
         if (error) throw error;
         resp.status(200).json(output.rows);
     });
 };
 const getBookByCat = (req, resp) => {
-    const cat = parseInt(req.params.cat);
+    const cat = parseInt(req.params.book_cat_id);
     pool.query(query.getBookByCat, [cat], (error,output) => {
         if (error) throw error;
         resp.status(200).json(output.rows);
     });
 };
 
+const addBook = (req, resp) => {
+    const {title,author,year,url,book_cat_id} = req.body;
+    //check if book already exist
+    pool.query(query.checkBookExist,[title],(error,output) => {
+        if (output.rows.length) {
+           resp.send("Title already exist"); 
+        }
+        //check if author exist
+        pool.query(query.checkAuthorExist,[author],(error,output) => {
+            const noAuthor = !output.rows.length
+            if (noAuthor) {
+                pool.query(query.addAuthor, [author], (error, output) => {
+                    if (error) throw error;
+                    pool.query(query.checkAuthorExist,[author],(error,output) => {
+                        for (let row of output.rows) {
+                            const author_id = row["author_id"] 
+                            pool.query(query.addBook, [title,author_id,year,url,book_cat_id], (error, output) => {
+                                if (error) throw error;
+                                resp.status(201).send("Book Added Succesfuly");
+                                console.log("Book Added")
+                            });                     
+                        };
+                    });
+                })
+            };
+            for (let row of output.rows) {
+                const author_id = row["author_id"] 
+                pool.query(query.addBook, [title,author_id,year,url,book_cat_id], (error, output) => {
+                    if (error) throw error;
+                    resp.status(201).send("Book Added Succesfuly");
+                    console.log("Book Added")
+                });
+            };
+        });
+    });   
+};
+
+const deleteBook = (req, resp) => {
+    const title = req.params.title;
+    //check if book already exist
+    pool.query(query.checkBookExist,[title],(error,output) => {
+        const noBookFound = !output.rows.length;
+        if (noBookFound) {
+            resp.send("You cannot delete Book. Book not found")
+        }  
+        pool.query(query.deleteBook,[title],(error,output) => {
+            if (error) throw error;
+            resp.status(200).send("Book Deleted Succesfuly");
+            console.log("Book Deleted")
+        })
+    });
+};
+
+
 module.exports = {
     getbookcat,
     getBookByCat,
+    addBook,
+    deleteBook,
 }
--- queries.js
+++ queries.js
@@ -1,10 +1,26 @@
-const getBooks = "SELECT * FROM r_books UNION ALL SELECT * FROM m_books UNION ALL SELECT * FROM c_books"
+const getBookByCat = 
+    "SELECT b.id,b.title,au.author_name,b.year,b.url,b.book_cat_id,al.book_cat FROM books b INNER JOIN all_books al USING (book_cat_id) INNER JOIN author au USING(author_id) WHERE b.book_cat_id = $1;";
+const getBookCat = 
+    "SELECT * FROM all_books INNER JOIN (SELECT b.book_cat_id, COUNT(*) FROM books b GROUP BY b.book_cat_id) AS book_tab USING (book_cat_id)";
+const checkBookExist = "SELECT b.title FROM books b WHERE b.title = $1";
+const checkAuthorExist = "SELECT author_id FROM author WHERE author_name = $1";
+const addAuthor = "INSERT INTO author (author_name) VALUES ($1)";
+const addBook = 
+    "INSERT INTO books (title,author_id,year,url,book_cat_id) VALUES ($1,$2,$3,$4,$5)";
+const deleteBook = "DELETE FROM books WHERE title = $1"
 
-const getBookByCat = "SELECT * FROM r_books INNER JOIN all_books USING (book_cat_id) WHERE book_cat_id = $1"
-
-const getBookCat = "SELECT * FROM all_books"
 
 module.exports = {
     getBookCat,
-    getBookByCat
+    getBookByCat,
+    checkBookExist,
+    checkAuthorExist,
+    addAuthor,
+    addBook,
+    deleteBook
 }
+
+
+
+
+
